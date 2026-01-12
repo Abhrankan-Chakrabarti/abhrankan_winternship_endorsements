5f8da35ea5170ccfe6f3092859a8ca5fc7c671ad
Abhrankan-Chakrabarti <abhrankan@gmail.com>
2026-01-13 00:45:54 +0530

    Fix API_BASE URL typo in config.js
    Corrected spelling of "winternship" in the Render deployment URL.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   API_BASE: location.hostname.includes("github.io")
-    ? "https://abhrankan-winernship-endorsements.onrender.com"
+    ? "https://abhrankan-winternship-endorsements.onrender.com"
     : "http://localhost:5001"
 };
