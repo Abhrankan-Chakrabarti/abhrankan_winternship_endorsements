6c5f1c46b019a855010836b331b878a0c73551e2
Abhrankan Chakrabarti <abhrankan@gmail.com>
2026-04-02 10:40:02 +0530

    Update API_BASE to include dpdns.org hostname

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  API_BASE: location.hostname.includes("github.io")
+  API_BASE: location.hostname.includes("github.io") || location.hostname.includes("dpdns.org")
     ? "https://abhrankan-winternship-endorsements.onrender.com"
     : "http://localhost:5001"
 };
